d0a59f2b287fbc156b0e5a35ff3326fc5b81515d
Ranaman196 <rana_ehtisham@hotmail.com>
2021-01-28 19:48:14 +0500

    map-continye

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// import { Company } from './Company';
+// import { User } from './User';
+
+// const user = new User();
+
+// console.log(user)
+
+// const company = new Company();
+
+// console.log(company)
+
