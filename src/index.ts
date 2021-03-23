353f47719384231ab8f7e5a38632e55248e5d54a
Ranaman196 <rana_ehtisham@hotmail.com>
2021-03-24 01:44:32 +0500

    reset the code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-// import { Company } from './Company';
-// import { User } from './User';
+import { Company } from "./Company";
+import { User } from "./User";
 
-// const user = new User();
+const user = new User();
 
-// console.log(user)
+console.log(user);
 
-// const company = new Company();
+const company = new Company();
 
-// console.log(company)
+console.log(company);
 
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
